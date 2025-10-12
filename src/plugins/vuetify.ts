f4be0b16af0fcd808ddfa2be88a80657da61ade9
Jordan Bradfield <jordan.p.bradfield@gmail.com>
2025-10-12 18:28:26 -0300

    Added base theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,8 +1,21 @@
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import { createVuetify } from "vuetify";
+import type { ThemeOptions } from "vuetify/lib/composables/theme.mjs";
+
+const theme: ThemeOptions = {
+  defaultTheme: "dark",
+  themes: {
+    dark: {
+      colors: {
+        primary: "#2962FF",
+      },
+    },
+  },
+};
 
 export default createVuetify({
   components,
   directives,
+  theme,
 });
