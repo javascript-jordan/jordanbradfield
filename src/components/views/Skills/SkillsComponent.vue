f3ce1d410b1fa33286d9468760cc90442d4a79a4
Jordan Bradfield <jordnbradfield.social@protonmail.com>
2026-04-10 17:16:49 -0300

    Added badges json data

--- src/components/views/Skills/SkillsComponent.vue
+++ src/components/views/Skills/SkillsComponent.vue
@@ -1,4 +1,20 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import type { BadgeMetadata, ICredlyBadgeData } from "@/interfaces";
+import constants from "@/util/constants";
+import CredlyBadges from "../../../../public/static/badges.json";
+
+const badges: ICredlyBadgeData = Object.keys(CredlyBadges).map((key: string) => {
+  const meta: BadgeMetadata = CredlyBadges[key];
+  return {
+    ...meta,
+    image: `${constants.EXTERNAL_LINKS.S3_BADEGS_PATH}${meta.name.replace(/ /g, "+")}.png`,
+  };
+});
+
+console.log(badges);
+
+console.log(badges);
+</script>
 <template>
   <h1>Skills</h1>
 </template>
--- src/util/constants.ts
+++ src/util/constants.ts
@@ -5,6 +5,7 @@ type IexternalLinks = {
 const EXTERNAL_LINKS: IexternalLinks = {
   LINKEDIN: "https://www.linkedin.com/in/jordan-bradfield-9333a1119",
   GITHUB: "https://github.com/javascript-jordan/jordanbradfield",
+  S3_BADEGS_PATH: "https://jordanbradfield.s3.us-east-1.amazonaws.com/images/badges/",
 };
 
 export default {
